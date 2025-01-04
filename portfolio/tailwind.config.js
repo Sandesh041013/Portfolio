de1167ad92c56fa2dab867f308c19987f68e41b5
Sandesh Acharya <sandeshacharya2003@gmail.com>
2025-01-04 20:25:26 +0545

    Updated the feature for better performance

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -9,23 +9,4 @@ export default {
   },
   plugins: [],
 }
-// tailwind.config.js
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the path to your project structure
-  ],
-  theme: {
-    extend: {
-      keyframes: {
-        slideIn: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        'slide-in': 'slideIn 1s ease-out',
-      },
-    },
-  },
-  plugins: [],
-};
+
